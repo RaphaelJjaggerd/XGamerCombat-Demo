c3700c4de0e5d70033314eeec18ae4df1f88f175
Raphael Jjaggerd <rjjaggerd@gmail.com>
2022-04-23 03:17:14 +0300

    Modified Scripatable Objects for guns

--- Assets/Scripts/ScriptableObjectGens/Gun.cs
+++ Assets/Scripts/ScriptableObjectGens/Gun.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Com.Kawaiisun.SimpleHostile
+namespace Com.RaphaelJjaggerd.XGC
 {
     [CreateAssetMenu(fileName = "New Gun", menuName = "Gun")]
     public class Gun : ScriptableObject
